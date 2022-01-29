9e17c6ce211a5f903053194b11813fc6ecdf57eb
Mariano Gobea Alcoba <gobeamariano@gmail.com>
2022-01-29 11:58:06 -0300

    Carga en github de curso POO y UML 29.1

---
+++ Python/main.py
@@ -0,0 +1,13 @@
+from car import Car # Importo la clase Car de mi file car para poder crear objetos concretos
+
+if __name__ == "__main__":
+    print("Hola Mundo")
+    car = Car() #Creo mi primer objeto car usando la clase car que ya defini en mi otro file
+    car.license = "AMS234" #Defino los atributos de objeto.
+    car.driver = "Andres Herrera" # ""
+    print(vars(car)) # El metodo vars se usa para imprimir todos los atributos del objeto creado
+
+    car2 = Car()
+    car2.license = "QWE567"
+    car2.driver = "Matha"
+    print(vars(car2))
